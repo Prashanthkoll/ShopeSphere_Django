b286aeeceebff244cb14f4cc79089913229b759c
prashanthkolla <prashanthkolla2003@gmail.com>
2026-08-13 13:54:40 +0530

    Fix Vercel build: allow pip install in externally-managed Python env

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "Installing dependencies..."
+python3.12 -m pip install --break-system-packages -r requirements.txt
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
+python3.12 manage.py collectstatic --noinput --clear
